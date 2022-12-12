9fc453584d6d5be951c8c0763ed8e496415458ca
Kimmy <119915681+kimmykomo@users.noreply.github.com>
2022-12-12 16:01:47 -0600

    added code

--- equality_operator.js
+++ equality_operator.js
@@ -7,3 +7,13 @@ function testEqual(val) {
 }
 
 testEqual(10);
+
+// strict operator
+function testStrict(val) {
+  if (val===7) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+testStrict(10);
