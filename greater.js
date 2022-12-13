631e5ea5101737755b6578441463e7b1d1130ec8
Kimmy <119915681+kimmykomo@users.noreply.github.com>
2022-12-13 09:38:15 -0600

    new files

--- greater.js
+++ greater.js
@@ -11,3 +11,19 @@ function testGreaterThan(val) {
 }
 
 testGreaterThan(10);
+
+//greater or equal
+
+function testGreaterOrEqual(val) {
+  if (val>=20) {  // Change this line
+    return "20 or Over";
+  }
+
+  if (val>=10) {  // Change this line
+    return "10 or Over";
+  }
+
+  return "Less than 10";
+}
+
+testGreaterOrEqual(10);
