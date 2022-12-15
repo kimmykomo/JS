055552a3c55e1246bfbeb344404323d442bd5ddc
Kimmy <119915681+kimmykomo@users.noreply.github.com>
2022-12-14 21:56:49 -0600

    Added new excersise

---
+++ switch_default.js
@@ -0,0 +1,28 @@
+function switchOfStuff(val) {
+  let answer = "";
+  // Only change code below this line
+switch (val){
+  case "a":
+    answer="apple";
+  break;
+
+  case "b":
+    answer="bird";
+    console.log(answer);
+  break;
+
+  case "c":
+    answer="cat";
+  break;
+
+  default:
+  answer= "stuff";
+  break;
+}
+
+
+  // Only change code above this line
+  return answer;
+}
+
+switchOfStuff("b");
