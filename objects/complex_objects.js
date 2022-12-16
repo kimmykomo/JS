dbeea8aa77443b8bea9d9497780862c5e42cd908
Kimmy <119915681+kimmykomo@users.noreply.github.com>
2022-12-15 21:41:28 -0600

    added new object files

---
+++ objects/complex_objects.js
@@ -0,0 +1,18 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [ "CD","8T","LP"],
+    "gold": true
+  },
+
+//add new complex object
+  {
+    "artist": "Abba",
+    "title" : "Gold",
+    "release_year" : 1980,
+    "formats": ["CD","LP"]
+  }
+  
+];
