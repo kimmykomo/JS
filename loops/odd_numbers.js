4cc67bad57c5fcc5150f8698dfd796187274df3f
Kimmy <119915681+kimmykomo@users.noreply.github.com>
2022-12-20 11:12:52 -0600

    new loops

---
+++ loops/odd_numbers.js
@@ -0,0 +1,14 @@
+// Setup
+const myArray = [];
+
+/*
+Push the odd numbers from 1 through 9 to myArray using a for loop.
+*/
+// Only change code below this line
+for (let i=1; i<10; i+=2){
+  myArray.push(i);
+}
+
+console.log(myArray);
+
+//now we will have 1, (then it will add +2 to the rest) 3, then 5, etc
